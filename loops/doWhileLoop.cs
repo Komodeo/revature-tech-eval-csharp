9b065b5cc9fa297ce6830678e4f6ddab3aa41272
Grant Brown <onlythebest@gmail.com>
2024-05-17 00:54:53 -0700

    loops

---
+++ loops/doWhileLoop.cs
@@ -0,0 +1,24 @@
+// Example do while loop
+
+using System;
+
+namespace Loops3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* do loop execution */
+            do
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a = a + 1;
+            }
+            while (a < 20);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ loops/forLoop.cs
@@ -0,0 +1,20 @@
+// Example for loop
+
+using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* for loop execution */
+            for (int a = 10; a < 20; a = a + 1)
+            {
+                Console.WriteLine("value of a: {0}", a);
+            }
+            //Console.ReadLine();
+        }
+    }
+}
