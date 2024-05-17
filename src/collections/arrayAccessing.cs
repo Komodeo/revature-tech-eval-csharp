69d81b63c7d26517cbbd6e729c79bd09a4b9dc05
Grant Brown <onlythebest@gmail.com>
2024-05-17 10:13:55 -0700

    array accessing

---
+++ src/collections/arrayAccessing.cs
@@ -0,0 +1,28 @@
+// Example accessing array elements
+
+using System;
+
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10]; /* n is an array of 10 integers */
+            int i, j;
+
+            /* initialize elements of array n */
+            for (i = 0; i < 10; i++)
+            { //Setting the array with values from 100 to 109
+                n[i] = i + 100;
+            }
+
+            /* output each array element's value */
+            for (j = 0; j < 10; j++)
+            { //Traversing through the array to display the values
+                Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+            }
+            //Console.ReadKey();
+        }
+    }
+}
